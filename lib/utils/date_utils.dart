1e7e1ec31785ce0879b74f61ba5be40e6ca2ef85
narawat.yahi <narawat.yahi@wmsl.local>
2023-01-12 14:41:52 +0700

    add date convert unit test

---
+++ lib/utils/date_utils.dart
@@ -0,0 +1,83 @@
+import 'package:intl/intl.dart';
+
+enum CalendarEnum { gregorian, buddhist }
+
+enum DateFormatEnum { shortDate, shortDateShortYear }
+
+extension DateFormatPattern on DateFormatEnum {
+  String get pattern {
+    switch (this) {
+      case DateFormatEnum.shortDate:
+        return 'dd/MM/yyyy';
+      case DateFormatEnum.shortDateShortYear:
+        return 'dd/MM/yy';
+    }
+  }
+}
+
+class DateUtils {
+  static const int _buddhistYearsOffset = 543;
+  static const String _slash = '/';
+
+  static String? convertToString(
+    DateTime? date,
+    DateFormatEnum format, [
+    CalendarEnum calendar = CalendarEnum.gregorian,
+  ]) {
+    if (date == null) {
+      return null;
+    }
+    final DateFormat dateFormat = DateFormat(format.pattern);
+    String dateString = dateFormat.format(date);
+    if (calendar == CalendarEnum.buddhist) {
+      List<String> split = dateString.split(_slash);
+      String day = split[0];
+      String month = split[1];
+      String shortYear = getShortYear(date, calendar);
+      String fullYear = getFullYear(date, calendar);
+      switch (format) {
+        case DateFormatEnum.shortDate:
+          dateString = '$day$_slash$month$_slash$fullYear';
+          break;
+        case DateFormatEnum.shortDateShortYear:
+          dateString = '$day$_slash$month$_slash$shortYear';
+          break;
+      }
+    }
+    return dateString;
+  }
+
+  static String getFullYear(DateTime date,
+      [CalendarEnum calendar = CalendarEnum.gregorian]) {
+    return getYearString(date, 'yyyy', calendar);
+  }
+
+  static String getShortYear(DateTime date,
+      [CalendarEnum calendar = CalendarEnum.gregorian]) {
+    return getYearString(date, 'yy', calendar);
+  }
+
+  static String getYearString(
+    DateTime date,
+    String yearFormat, [
+    CalendarEnum calendar = CalendarEnum.gregorian,
+  ]) {
+    final DateFormat dateFormat = DateFormat(yearFormat);
+    switch (calendar) {
+      case CalendarEnum.gregorian:
+        return dateFormat.format(date);
+      case CalendarEnum.buddhist:
+        return dateFormat.format(DateTime(getYear(date, calendar)));
+    }
+  }
+
+  static int getYear(DateTime date,
+      [CalendarEnum calendar = CalendarEnum.gregorian]) {
+    switch (calendar) {
+      case CalendarEnum.gregorian:
+        return date.year;
+      case CalendarEnum.buddhist:
+        return date.year + _buddhistYearsOffset;
+    }
+  }
+}
---
+++ test/date_test.dart
@@ -0,0 +1,67 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:testing_flutter_apps/utils/date_utils.dart';
+
+void main() {
+  test('Convert date to string in Gregorian calendar', () {
+    // 01-01-1989
+    String? result1 = DateUtils.convertToString(DateTime(1989, 9, 20), DateFormatEnum.shortDate);
+    // 29-02-2020
+    String? result2 = DateUtils.convertToString(DateTime(2020, 2, 29), DateFormatEnum.shortDate);
+    // 01-01-2023
+    String? result3 = DateUtils.convertToString(DateTime(2023, 1, 1), DateFormatEnum.shortDateShortYear);
+    // 07-08-2000
+    String? result4 = DateUtils.convertToString(DateTime(2000, 8, 7), DateFormatEnum.shortDateShortYear);
+
+    expect(result1, '20/09/1989');
+    expect(result2, '29/02/2020');
+    expect(result3, '01/01/23');
+    expect(result4, '07/08/00');
+  });
+  test('Convert date to string in Buddhist calendar', () {
+    // 01-01-1989
+    String? result1 = DateUtils.convertToString(DateTime(1989, 9, 20), DateFormatEnum.shortDate, CalendarEnum.buddhist);
+    // 29-02-2020
+    String? result2 = DateUtils.convertToString(DateTime(2020, 2, 29), DateFormatEnum.shortDate, CalendarEnum.buddhist);
+    // 01-01-2023
+    String? result3 = DateUtils.convertToString(DateTime(2023, 1, 1), DateFormatEnum.shortDateShortYear, CalendarEnum.buddhist);
+    // 07-08-2000
+    String? result4 = DateUtils.convertToString(DateTime(2000, 8, 7), DateFormatEnum.shortDateShortYear, CalendarEnum.buddhist);
+
+    expect(result1, '20/09/2532');
+    expect(result2, '29/02/2563');
+    expect(result3, '01/01/66');
+    expect(result4, '07/08/43');
+  });
+  // group('Convert date to string', () {
+  //   test('In Gregorian calendar', () {
+  //     // 01-01-1989
+  //     String? result1 = DateUtils.convertToString(DateTime(1989, 9, 20), DateFormatEnum.shortDate);
+  //     // 29-02-2020
+  //     String? result2 = DateUtils.convertToString(DateTime(2020, 2, 29), DateFormatEnum.shortDate);
+  //     // 01-01-2023
+  //     String? result3 = DateUtils.convertToString(DateTime(2023, 1, 1), DateFormatEnum.shortDateShortYear);
+  //     // 07-08-2000
+  //     String? result4 = DateUtils.convertToString(DateTime(2000, 8, 7), DateFormatEnum.shortDateShortYear);
+  //
+  //     expect(result1, '20/09/1989');
+  //     expect(result2, '29/02/2020');
+  //     expect(result3, '01/01/23');
+  //     expect(result4, '07/08/00');
+  //   });
+  //   test('In Buddhist calendar', () {
+  //     // 01-01-1989
+  //     String? result1 = DateUtils.convertToString(DateTime(1989, 9, 20), DateFormatEnum.shortDate, CalendarEnum.buddhist);
+  //     // 29-02-2020
+  //     String? result2 = DateUtils.convertToString(DateTime(2020, 2, 29), DateFormatEnum.shortDate, CalendarEnum.buddhist);
+  //     // 01-01-2023
+  //     String? result3 = DateUtils.convertToString(DateTime(2023, 1, 1), DateFormatEnum.shortDateShortYear, CalendarEnum.buddhist);
+  //     // 07-08-2000
+  //     String? result4 = DateUtils.convertToString(DateTime(2000, 8, 7), DateFormatEnum.shortDateShortYear, CalendarEnum.buddhist);
+  //
+  //     expect(result1, '20/09/2532');
+  //     expect(result2, '29/02/2563');
+  //     expect(result3, '01/01/66');
+  //     expect(result4, '07/08/43');
+  //   });
+  // });
+}
